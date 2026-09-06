4d976d6b21056e8d5ac768917ab373c5db483230
Pedrogg108 <pedroalancay2018@gmail.com>
2026-09-06 00:04:59 -0300

    seccion novedades en  trabajo

--- proyectopm/src/app/paginas/novedades/novedades.page.ts
+++ proyectopm/src/app/paginas/novedades/novedades.page.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FootbarPage } from 'src/app/compartido/footbar/footbar.page';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar,IonCard,IonCardContent,IonCardHeader,IonCardSubtitle,IonCardTitle } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-novedades',
   templateUrl: './novedades.page.html',
   styleUrls: ['./novedades.page.scss'],
   standalone: true,
-  imports: [IonContent,FootbarPage, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent,FootbarPage, IonHeader, IonTitle,IonCard,IonCardContent,IonCardHeader,IonCardSubtitle,IonCardTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class NovedadesPage implements OnInit {
 
